250b9399877aaf7809597cdbdbfb7a374fec0e46
etalox <etalox@users.noreply.github.com>
2026-09-22 22:58:49 +0000

    Deploy site to gh-pages via GitHub Actions e03f63c427c5aa864692e23df4ce7fd314907823

--- assets/Thumbnails/brucolastic-manifest.js
+++ assets/Thumbnails/brucolastic-manifest.js
@@ -2,18 +2,18 @@
 window.BRUCOLASTIC_THUMBNAILS = [
   {
     "file": "Thumbnail_82941.jpeg",
-    "modified": 1790115995440
+    "modified": 1790117922519
   },
   {
     "file": "Thumbnail_82194.png",
-    "modified": 1790115995427
-  },
-  {
-    "file": "Thumbnail_78295.png",
-    "modified": 1790115995424
+    "modified": 1790117922511
   },
   {
     "file": "Thumbnail(12) (7).png",
-    "modified": 1790115995423
+    "modified": 1790117922509
+  },
+  {
+    "file": "Thumbnail_78295.png",
+    "modified": 1790117922509
   }
 ];
